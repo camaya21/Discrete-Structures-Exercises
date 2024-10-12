5efd304c7b3658ebf5d7c86c98013ae5a79247ed
camaya21 <cindymamaya21@gmail.com>
2024-10-12 13:49:12 -0400

    Method to calculate factorial

--- FactorialFunction.java
+++ FactorialFunction.java
@@ -1,4 +1,14 @@
 import java.math.BigInteger;
 public class FactorialFunction {
     
+    public static BigInteger factorial(int n){
+        BigInteger result = BigInteger.ONE; //Initializes variable names result to the value of 1
+        for (int i = 2; i<=n; i++){
+            result = result.multiply(BigInteger.valueOf(i));
+        }
+        return result;
+    }
+
+    
+
 }
