f8a7388ceea5520701e9291e177a84adfc4f3326
camaya21 <cindymamaya21@gmail.com>
2024-10-12 13:59:44 -0400

    Calculate and Print possibilites

--- FactorialFunction.java
+++ FactorialFunction.java
@@ -1,3 +1,9 @@
+/*
+QUESTION:
+A group 12 students go one a hike, accompanied by two faculty members who are the group leaders. 
+How many ways are there for the entire group (including students and leaders) to line up? 
+*/
+
 import java.math.BigInteger;
 public class FactorialFunction {
     
@@ -15,5 +21,14 @@ public class FactorialFunction {
         return factorial(totalIndividuals);
     }
 
+    public static void main(String[] args) {
+        int numStudents = 12;
+        int numFaculty = 2;
+
+        // Calculate and print the total arrangements
+        BigInteger possibilities = totalPeople(numStudents, numFaculty);
+        System.out.println("The total number of ways for the group to line up is: " + possibilities);
+    }
 
 }
+
